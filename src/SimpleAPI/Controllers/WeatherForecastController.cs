3b10f6e18275223bad595c4d48a53d607cd7a880
P. S. Kuma (c) <p.s.kuma@avanade.com>
2021-02-21 15:24:34 +0000

    Second Version change comments

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Something Else";
+            return "Else to Do with ";
         }
 
 
